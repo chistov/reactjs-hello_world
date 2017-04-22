c16fd52ee622175665e699fb16d23dd8310009c1
Sergey Chistov <s.chistov@norsi-trans.ru>
2017-04-22 13:17:24 +0300

    initial . v. 1.0

---
+++ webpack.config.js
@@ -0,0 +1,54 @@
+var webpack = require('webpack');
+var path = require('path');
+
+var BUILD_DIR = path.resolve(__dirname, 'src/client/public/build');
+var APP_DIR = path.resolve(__dirname, './');
+
+var config = {
+    entry: APP_DIR + '/index.js',
+      output: {
+      path: BUILD_DIR,
+      filename: 'bundle.js'
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.js$/,
+                loader: "babel-loader",
+                exclude: [/node_modules/, /public/]
+            },
+            {
+                test: /\.css$/,
+                loader: "style-loader!css-loader!autoprefixer-loader",
+                exclude: [/node_modules/, /public/]
+            },
+            {
+                test: /\.gif$/,
+                loader: "url-loader?limit=10000&mimetype=image/gif"
+            },
+            {
+                test: /\.jpg$/,
+                loader: "url-loader?limit=10000&mimetype=image/jpg"
+            },
+            {
+                test: /\.png$/,
+                loader: "url-loader?limit=10000&mimetype=image/png"
+            },
+            {
+                test: /\.svg/,
+                loader: "url-loader?limit=26000&mimetype=image/svg+xml"
+            },
+            {
+                test: /\.jsx$/,
+                loader: "react-hot-loader!babel-loader",
+                exclude: [/node_modules/, /public/]
+            },
+            {
+                test: /\.json$/,
+                loader: "json-loader"
+            }
+        ]
+    }
+};
+
+module.exports = config;
